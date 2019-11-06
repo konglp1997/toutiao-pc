b7193e37faa458fca04b7265fbe8074a1a046c34
konglp1997 <konglp1997@163.com>
2019-11-06 17:40:55 +0800

    粉丝管理完成

--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -1,9 +1,117 @@
 <template>
-  <div class="contai254ner">2</div>
+  <el-card>
+    <div slot="header">
+      <my-bread>粉丝管理</my-bread>
+    </div>
+    <el-tabs v-model="activeName" type="card">
+      <el-tab-pane label="粉丝列表" name="list">
+        <div class="fans_list">
+          <div class="fans_item" v-for="item in fanslist" :key="item.id.toString()">
+            <el-avatar :size="80" :src="item.photo"></el-avatar>
+            <p>{{item.name}}</p>
+            <el-button type="primary" size="small" plain>+关注</el-button>
+          </div>
+        </div>
+        <!-- 分页 -->
+        <el-pagination background layout="prev, pager, next" :total="total" @current-change="pager"></el-pagination>
+      </el-tab-pane>
+      <el-tab-pane label="粉丝画像" name="pic">
+        <div ref="dom" style="width: 600px;height:400px;"></div>
+      </el-tab-pane>
+    </el-tabs>
+  </el-card>
 </template>
 
 <script>
-export default {}
+import defaultImage from '../../assets/avatar.jpg'
+import echarts from 'echarts'
+
+export default {
+  data () {
+    return {
+      activeName: 'pic',
+      defaultImage,
+      fanslist: [],
+      reqParams: {
+        page: 1,
+        per_page: 21
+      },
+      total: 0
+    }
+  },
+  created () {
+    this.getFanslist()
+  },
+  methods: {
+    async getFanslist () {
+      const {
+        data: { data }
+      } = await this.$axios.get('followers', { params: this.reqParams })
+      this.fanslist = data.results
+      this.total = data.total_count
+    },
+    pager (newPage) {
+      this.reqParams.page = newPage
+      this.getFanslist()
+    }
+  },
+  mounted () {
+    const dom = this.$refs.dom
+    const myEcharts = echarts.init(dom)
+    const option = {
+      color: ['#3398DB'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '直接访问',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
+        }
+      ]
+    }
+
+    myEcharts.setOption(option)
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.fans_list {
+  .fans_item {
+    width: 150px;
+    height: 190px;
+    text-align: center;
+    border: 1px dashed #ddd;
+    padding-top: 20px;
+    display: inline-block;
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
+}
+</style>
